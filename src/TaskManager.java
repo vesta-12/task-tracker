aee13aa2acf99bbc4028f645726e00896d18100c
vesta-12 <sofiya.serbina@narxoz.kz>
2026-02-17 10:08:47 +0500

    Add task deletion command
    WIP: list tasks

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,3 +1,14 @@
 public class TaskManager {
+    public static void createTask(String title) {
+        System.out.println("Task '" + title + "' created");
+    }
+
+    public static void listTasks() {
+        System.out.println("Listing tasks...");
+    }
+
+        public static void deleteTask(String title) {
+            System.out.println("Task '" + title + "' deleted");
+        }
 
 }
